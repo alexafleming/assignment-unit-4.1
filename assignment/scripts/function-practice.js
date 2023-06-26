9906fea1581059a20feb45df9297af5b0110571d
Alexa Fleming <alexaflemingmn@gmail.com>
2023-06-26 15:22:24 -0500

    assignment 4

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -23,24 +23,28 @@ function helloName(firstName) {
 console.log(helloName("Alexa"));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+   return firstNumber + secondNumber;
+  
 }
-
+console.log(addNumbers(7, 9));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
+function multiplyThree(numOne, numTwo, numThree) {
+  return numOne * numTwo * numThree;
 
 }
+
+console.log(multiplyThree(1, 10, 20));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -52,16 +56,27 @@ console.log('isPositive - should say false', isPositive(-3));
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+  return array[array.length-1];
 }
+
+console.log(getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
+  for (const currentItem of array) {
+    console.log(currentItem);
 
+    if(currentItem === value){
+      return true;
+    }
+  }
+
+  return false;
 }
 
+console.log(find("cat", ["cat", "dog", "mouse"]))
 // ----------------------
 // Stretch Goals
 // ----------------------
